cae73573578a51edf8e064f7ead9dc91fb341a17
Erwin Eggenberger <oss-2015@keyboardrocker.com>
2015-11-14 21:52:46 +0100

    Removed old test script

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 svdrp = require('./lib/svdrpclient');
 client = new svdrp.svdrpclient();
 //client = new svdrp.svdrpclient('localhost', 6419);
-client.listTimers();
+//client.listTimers();
+//client.listChannels( console.log );
+//client.listChannels( console.log, 1 );
 //client.nextTimer();
 //client.nextTimer('abs');
-//client.nextTimer('rel');
+client.nextTimer( console.log, 'rel');
